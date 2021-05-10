5e13d0a9641ee158b28b619ef4a780716d858981
Yong-Hsu <34927393+Yong-Hsu@users.noreply.github.com>
2021-05-10 21:59:20 +0800

    comb_2

--- app/__init__.py
+++ app/__init__.py
@@ -1,17 +1,7 @@
 from flask import Flask
-from sar import main
-import numpy as np
 
 app = Flask(__name__)
 
 from app import views
 
 
-@app.route('/servCheck', methods=['GET', 'POST'])
-def service_status():
-    return {'status': '1'}
-
-
-@app.route('/608test', methods=['GET', 'POST'])
-def matrix608_send():
-    return {'matrix_prob': main.matrix_prob_608()}
--- app/views.py
+++ app/views.py
@@ -1,7 +1,18 @@
 from flask import render_template
 from app import app
+from sar import main
 
 
 @app.route('/')
 def home():
     return render_template("index.html")
+
+
+@app.route('/servCheck', methods=['GET', 'POST'])
+def service_status():
+    return {'status': '1'}
+
+
+@app.route('/608test', methods=['GET', 'POST'])
+def matrix608_send():
+    return {'matrix_prob': main.matrix_prob_608()}
